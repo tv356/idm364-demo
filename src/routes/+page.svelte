ae3c1da74156a21ffefe1218718e25e8d85e948b
Tran (Cheryl) Vo <71985185+tv356@users.noreply.github.com>
2025-03-22 15:12:32 +0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script>
-  export let products = []; // Receive products from the server
-  let featuredCollection = products.slice(0, 3); // Get the first 3 products as featured
+  import ThreeItemGrid from '$components/ThreeItemGrid.svelte';
+  import Carousel from '$components/Carousel.svelte';
+
+  /** @type {{data: import('./$types').PageData}} */
+  let { data } = $props();
+
+  let clothesCollection = $derived(data.products[0]?.node?.products?.edges);
+  let featuredCollection = $derived(data.products[1]?.node?.products?.edges);
 </script>
 
 <svelte:head>
@@ -13,19 +19,23 @@
       <ThreeItemGrid products={featuredCollection} />
     </div>
   </section>
-
   <section>
-    <Carousel items={featuredCollection} />
+    <Carousel items={clothesCollection} />
   </section>
-
   <section>
-    <div class="flex flex-col px-8 py-20 text-black border border-gray-300 bg-white lg:flex-row lg:items-center">
-      <div class="flex-none mb-4 mr-8 text-3xl font-black text-left md:text-4xl lg:mb-0 lg:w-1/3 lg:text-right lg:text-6xl">
+    <div
+      class="flex flex-col px-8 py-20 text-black border border-gray-300 bg-white lg:flex-row lg:items-center"
+    >
+      <div
+        class="flex-none mb-4 mr-8 text-3xl font-black text-left md:text-4xl lg:mb-0 lg:w-1/3 lg:text-right lg:text-6xl"
+      >
         Dessert dragée halvah croissant.
       </div>
       <div>
         <div class="lg:text-2xl text-black">
-          Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans. Tiramisu sweet roll cheesecake pie carrot cake.
+          Cupcake ipsum dolor sit amet lemon drops pastry cotton candy. Sweet carrot cake macaroon
+          bonbon croissant fruitcake jujubes macaroon oat cake. Soufflé bonbon caramels jelly beans.
+          Tiramisu sweet roll cheesecake pie carrot cake.
         </div>
         <button class="mt-4 font-bold text-svelteOrange hover:text-svelteDark lg:text-2xl">
           Read it here
